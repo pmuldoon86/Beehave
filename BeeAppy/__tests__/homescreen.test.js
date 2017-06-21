04c75d32f8a986f81afdedd6b6b08fabfa6d2910
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-06-21 15:16:00 +0100

    Add Enzyme test for Home component

--- BeeAppy/__tests__/homescreen.test.js
+++ BeeAppy/__tests__/homescreen.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { shallow, mount, render } from 'enzyme';
 import Home from '../src/Home';
-
 import renderer from 'react-test-renderer';
 
-it('renders a home page', () => {
-  const homepage = renderer.create(<Home/>).toJSON();
-  expect(homepage).toMatchSnapshot();
+describe('Home component', function() {
+  it('renders a home page', () => {
+    const homepage = renderer.create(<Home/>).toJSON();
+    expect(homepage).toMatchSnapshot();
+  });
+
+  it('should render', function() {
+    expect(render(<Home/ >).text()).toEqual('Hi');
+  });
 });
