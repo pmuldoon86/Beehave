c8fc88c4ecf48e69a3059aa6e771fb5ab3c43a72
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-06-21 20:47:49 +0100

    Add Donation page, accessible by button on Home page

--- BeeAppy/App.js
+++ BeeAppy/App.js
@@ -11,16 +11,31 @@ class HomeScreen extends React.Component {
     title: 'BeeAppy',
   };
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View>
+      <Button
+        onPress={() => navigate('Donate')}
+        title='Link to Donation page'
+      />
         <Text>Home page content</Text>
       </View>
     );
   }
 }
 
+class Donate extends React.Component {
+  static navigationOptions = {
+    title: 'Donation page',
+  };
+  render() {
+    return <Text>Donation content</Text>
+  }
+}
+
 const SimpleApp = StackNavigator({
   Home: { screen: HomeScreen },
+  Donate: { screen: Donate },
 });
 
 export default SimpleApp;
