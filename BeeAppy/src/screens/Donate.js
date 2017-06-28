29ad8d4b6b0dd5a785004468caa40ca232d778a3
Paula Muldoon <pmuldoon@umich.edu>
2017-06-28 18:28:31 +0100

    donate screen links to home

--- BeeAppy/src/screens/Donate.js
+++ BeeAppy/src/screens/Donate.js
@@ -7,15 +7,17 @@ import {
   TouchableOpacity,
   Image
 } from 'react-native';
+import BeeButton from '../components/buttons/BeeButton'
+import BeeFactButton from '../components/buttons/BeeFactButton'
 import style from '../styles/style.js';
 import StackNavigator from 'react-navigation';
 
 export default class Donate extends React.Component {
-  static navigationOptions = {
-    title: 'Donation page',
-  };
+
   render() {
+    const { navigate } = this.props.navigation;
     return (
+
       <View style={style.viewStyle}>
         <View style={style.donateListStyle}>
           <TouchableHighlight onPress={() => Linking.openURL('https://bumblebeeconservation.org/')}>
@@ -31,6 +33,28 @@ export default class Donate extends React.Component {
             <Text style={style.donateText}>Adopt a Bumblebee{'\n'}</Text>
           </TouchableOpacity>
         </View>
+
+        <TouchableHighlight
+          onPress={() => navigate('Home')}
+          style={style.donateButton}>
+          <Image
+            style={style.imageButtonStyle}
+            source={require('../img/beehive.png')}
+          />
+        </TouchableHighlight>
+
+          <TouchableHighlight
+            onPress={() => navigate('BeeHive')}
+            style={style.beehiveButton}>
+            <Image
+              style={style.imageButtonStyle}
+              source={require('../img/earth-globe.png')}
+            />
+          </TouchableHighlight>
+
+          <View style={style.beeFactButton}>
+            <BeeFactButton/>
+          </View>
       </View>
     )
   }
